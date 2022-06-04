cc2884f790280c35e9dad171324033d4061bdac3
jeong-soo <wjdtn4383@daum.net>
2022-06-05 00:37:45 +0900

    init setup

---
+++ src/main/java/dev/jsoo/trranslationapiserver/TrranslationApiServerApplication.java
@@ -0,0 +1,13 @@
+package dev.jsoo.trranslationapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrranslationApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrranslationApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jsoo/trranslationapiserver/TrranslationApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jsoo.trranslationapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrranslationApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
